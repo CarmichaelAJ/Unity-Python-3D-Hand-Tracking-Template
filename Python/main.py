2b3138118fc68bb80aec9a92b8aeb220be538cab
AJ <CarmichaelAJ@outlook.com>
2023-04-04 08:22:14 -0400

    Python Unity
    3D Hand Tracking

---
+++ Python/main.py
@@ -0,0 +1,43 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import socket 
+
+# Paramaters
+width, height = 1280, 720
+
+# webcam
+cap = cv2.VideoCapture(0)
+cap.set(3,width)
+cap.set(4,height)
+
+# Hand Detector
+detector = HandDetector(maxHands=1, detectionCon=0.8)
+
+# Communication
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# linking python to unity through a locally created server
+serverAddressPort = ("127.0.0.1", 5052) 
+
+while True:
+    # Get the frame from the webcam
+    success, img = cap.read()
+    # Hands
+    hands, img = detector.findHands(img)
+
+    data = []
+    # Landmark values = (x, y, z) * 21
+    if hands:
+        # Get the first hand detected
+        hand = hands[0]
+        # Get the landmark list
+        lmList = hand['lmList']
+        print(lmList) ## exporting coordinates as a string for C#
+        for lm in lmList:
+            data.extend([lm[0], height - lm[1], lm[2]])
+        print(data)
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+        
+    img = cv2.resize(img,(0,0),None, 0.25, 0.25)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
